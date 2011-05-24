aeae13f1e8b87b67333f392d44c8c190eff0b426
Andrey Khalyavin <halyavin@google.com>
2011-05-24 10:41:21 +0400

    Ignore parameter of __exit_thread which is always zero anyway.
    R=mseaborn@chromium.org
    BUG=none
    TEST=http://codereview.chromium.org/7039039/
    
    Review URL: http://codereview.chromium.org/7004032

--- sysdeps/nacl/exit-thread.c
+++ sysdeps/nacl/exit-thread.c
@@ -1,9 +1,9 @@
 #include <nacl_syscalls.h>
-#include <unistd.h>	
+#include <unistd.h>
 
 void __exit_thread (int val)
 {
-  NACL_SYSCALL (thread_exit) (val);
+  NACL_SYSCALL (thread_exit) (NULL);
   /* Not reached */
   __asm__ ("hlt");
 }
