c9f89561edf37e01c14cae1d41391db9fb772147
Andrey Khalyavin <halyavin@google.com>
2011-05-25 11:28:33 +0400

    Fix join with main thread bug.
    We need to wake futex pthread_join is sleeping in. The code is the same as in the end of start_thread function in pthread_create.c.
    
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=1831
    TEST=glibc tests
    
    R=khim@google.com,pasko@google.com,mseaborn@chromium.org
    
    Review URL: http://codereview.chromium.org/7066017

--- sysdeps/nacl/exit-thread.c
+++ sysdeps/nacl/exit-thread.c
@@ -1,9 +1,15 @@
+#include <atomic.h>
 #include <nacl_syscalls.h>
+#include <nptl/pthreadP.h>
 #include <unistd.h>
 
 void __exit_thread (int val)
 {
-  NACL_SYSCALL (thread_exit) (NULL);
+  /* We are about to die: make our pd "almost free" and wake up waiter. */
+  struct pthread* pd = THREAD_SELF;
+  pd->tid = -2;
+  __nacl_futex_wake (&pd->tid, INT_MAX, __FUTEX_BITSET_MATCH_ANY);
+  NACL_SYSCALL (thread_exit) (&pd->tid);
   /* Not reached */
   __asm__ ("hlt");
 }
