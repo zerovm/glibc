84d7d30ad4ef93e9bf2beb34bcf773f3c542b21e
Victor Khimenko <khim@chromium.org>
2011-08-09 14:57:46 +0400

    Fix __brk function (vital for static linking).
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=2112
    TEST=glibc_tests
    
    R=pasko@google.com
    
    Review URL: http://codereview.chromium.org/7602013

--- sysdeps/nacl/brk.c
+++ sysdeps/nacl/brk.c
@@ -11,15 +11,16 @@ weak_alias (__curbrk, ___brk_addr)
 
 int __brk (void *addr)
 {
-  int rv = __nacl_irt_sysbrk (&__curbrk);
+  int rv = __nacl_irt_sysbrk (&addr);
   if (rv != 0) {
     errno = rv;
     return -1;
   }
-  if (__curbrk < addr) {
+  if (__curbrk > addr) {
     errno = ENOMEM;
     return -1;
   }
+  __curbrk = addr;
   return 0;
 }
 weak_alias (__brk, brk)
