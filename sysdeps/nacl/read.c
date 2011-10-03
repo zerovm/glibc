f2679f9c9acdf00ed69480e37c2c076e5d284b82
Andrey Khalyavin <halyavin@google.com>
2011-10-03 21:30:54 +0400

    Fix error codes in read and sched_yield.
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=2329
    TEST= none
    
    R=khim@google.com
    
    Review URL: http://codereview.chromium.org/8116004

--- sysdeps/nacl/read.c
+++ sysdeps/nacl/read.c
@@ -11,7 +11,7 @@ ssize_t __libc_read (int fd, void *buf, size_t size)
   size_t nread;
   int result = __nacl_irt_read (fd, buf, size, &nread);
   if (result != 0) {
-    errno = -result;
+    errno = result;
     return -1;
   }
   return nread;
--- sysdeps/nacl/sched_yield.c
+++ sysdeps/nacl/sched_yield.c
@@ -9,10 +9,10 @@ int __sched_yield (void)
 {
   int result = __nacl_irt_sched_yield ();
   if (result != 0) {
-    errno = -result;
+    errno = result;
     return -1;
   }
-  return -result;
+  return 0;
 }
 libc_hidden_def (__sched_yield)
 weak_alias (__sched_yield, sched_yield)
