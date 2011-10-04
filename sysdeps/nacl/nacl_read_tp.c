36d20ec43ee522cfd9839f90546e22a399cef1e8
Evgeny Eltsin <eaeltsin@chromium.org>
2011-10-04 16:54:44 -0700

    Introduce __nacl_add_tp
    __nacl_add_tp returns thread pointer plus an offset passed in the first argument.
    
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=237
    TEST=
    
    Review URL: http://codereview.chromium.org/8143011

--- sysdeps/nacl/nacl_read_tp.c
+++ sysdeps/nacl/nacl_read_tp.c
@@ -7,3 +7,27 @@ void* __attribute__ ((const)) __nacl_read_tp ()
   return __nacl_irt_tls_get ();
 }
 #endif
+
+#ifdef __x86_64__
+/* NaCl imposes severe limitations on call instruction alignment. This implies
+   we have to keep calls on the same position when doing TLS rewrites.
+   Unfortunately, dynamic TLS models call __tls_get_addr at the end, while exec
+   TLS models call __nacl_read_tp at the beginning.
+
+   __nacl_add_tp returns thread pointer plus an offset passed in the first
+   argument. Using __nacl_add_tp we can create exec TLS models code sequences
+   with call at the end, thus enabling dynamic to exec TLS rewrites.
+
+   For example, __nacl_read_tp initial exec code sequence is:
+     call __nacl_read_tp
+     add x@gottpoff(%rip),%eax
+
+   And __nacl_add_tp equivalent is:
+     mov x@gottpoff(%rip),%edi  #  first integer argument is passed in %rdi
+     call __nacl_add_tp  */
+
+void* __nacl_add_tp (ptrdiff_t off)
+{
+  return (char*) __nacl_irt_tls_get () + off;
+}
+#endif
