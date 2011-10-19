40b700e41961ef2c8d046c8eac05824165efc16a
Victor Khimenko <khim@chromium.org>
2011-10-20 00:34:30 +0400

    Don't use Linux code in sysconf(_SC_NPROCESSORS_ONLN)
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=2202
    TEST=earth (look for "Using 16 processors based on sysconf" message)
    
    R=pasko@google.com
    
    Review URL: http://codereview.chromium.org/8339017

---
+++ sysdeps/nacl/getsysstats.c
@@ -0,0 +1,40 @@
+#include <errno.h>
+#include <irt_syscalls.h>
+
+/* Return the number of processors configured on the system. */
+int
+__get_nprocs_conf ()
+{
+  int result = 1;
+
+  if (__nacl_irt_sysconf (1 /*NACL_ABI__SC_NPROCESSORS_ONLN*/, &result))
+    return 1;
+  return result;
+}
+
+/* Return the number of processors currently available on the system. */
+int
+__get_nprocs ()
+{
+  int result = 1;
+
+  if (__nacl_irt_sysconf (1 /*NACL_ABI__SC_NPROCESSORS_ONLN*/, &result))
+    return 1;
+  return result;
+}
+
+/* Return the number of physical pages on the system. */
+long int
+__get_phys_pages ()
+{
+  errno = ENOSYS;
+  return -1;
+}
+
+/* Return the number of available physical pages */
+long int
+__get_avphys_pages ()
+{
+  errno = ENOSYS;
+  return -1;
+}
