96da47388b9160e17473670d4269ba1cc9ff3fd0
YaroslavLitvinov <yaroslav.litvinov@gmail.com>
2013-12-26 21:19:38 +0200

    errno fixed for lseek

--- sysdeps/nacl/lseek.c
+++ sysdeps/nacl/lseek.c
@@ -9,11 +9,10 @@ off_t __lseek (int fd, off_t offset, int whence)
 {
   nacl_abi_off_t nacl_offset = offset;
   int result = __nacl_irt_seek (fd, nacl_offset, whence, &nacl_offset);
-  if (result != 0) {
-    errno = result;
-    return -1;
-  }
-  return nacl_offset;
+  if (result != 0)
+      return -1;
+  else
+      return nacl_offset;
 }
 libc_hidden_def (__lseek)
 weak_alias (__lseek, lseek)
