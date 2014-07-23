fea041673c4766bfb40d01c9eb25df83e264fb45
YaroslavLitvinov <yaroslav.litvinov@gmail.com>
2014-07-23 20:43:10 +0300

    __ZRT_HOST support: pagesize=4K is hardcoded, it's the same as used on ubuntu linux

--- sysdeps/nacl/getpagesize.c
+++ sysdeps/nacl/getpagesize.c
@@ -8,8 +8,11 @@ int __getpagesize ()
 {
   /* NaCl does not have a syscall for getting the page size.  The page
      size is fixed at 64k for compatiblity with Windows. */
+#ifdef __ZRT_HOST
+  return 0x1000;
+#else
   return 0x10000;
-  //return 0x1000;
+#endif
 }
 libc_hidden_def (__getpagesize)
 weak_alias (__getpagesize, getpagesize)
