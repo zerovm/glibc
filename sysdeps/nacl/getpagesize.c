bef28c7be6da2b0d275f87230f8e83131a606be8
YaroslavLitvinov <yaroslav.litvinov@gmail.com>
2014-07-27 20:59:08 +0300

    one pagesize is used bot both nacl and host builds

--- sysdeps/nacl/getpagesize.c
+++ sysdeps/nacl/getpagesize.c
@@ -8,11 +8,7 @@ int __getpagesize ()
 {
   /* NaCl does not have a syscall for getting the page size.  The page
      size is fixed at 64k for compatiblity with Windows. */
-#ifdef __ZRT_HOST
-  return 0x1000;
-#else
   return 0x10000;
-#endif
 }
 libc_hidden_def (__getpagesize)
 weak_alias (__getpagesize, getpagesize)
